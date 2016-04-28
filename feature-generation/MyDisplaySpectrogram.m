1f159d03ab08de449ea579a03d7d7fd0aae44985
Sandro Giacomuzzi <Giaco@sandros-macbook-pro.home>
2016-04-28 23:10:01 +0200

    Bug im Spektrogramplotter entfernt und die Features weren nun richtigerweise vor der Rekonstruierung generiert

--- feature-generation/MyDisplaySpectrogram.m
+++ feature-generation/MyDisplaySpectrogram.m
@@ -1,45 +1,12 @@
 
 function MyDisplaySpectrogram(spec, fs, frameStep)
-% function DisplaySpectrogram(spec, fs, frameStep, enhanceHighFrequencies)
-% Display a (potentially) complex-valued, positive-and-negative-frequency
-% spectrogram in the normal format.
-% spec - a spectrogram with ONLY positive frequencies.  Each column
-% contains the spectral information for
-% one frame's worth of data.
-% fs - Sample rate for the original waveform data
-% frameStep - The number of samples between spectrogram frames.
-% enhanceHighFrequencies - Modify the the spectrogram to enhance high
-% frequencies.  This is often useful because speech has a 1/f spectrum, and
-% by multiplying each spectral bin by its frequency we can counteract this.
-% High-frequency enhancement is the default when using DisplaySpectrogram.
-
-% Written by Malcolm Slaney, Microsoft Research, July 2014
-
-
-Fs=fs;
-fs=floor(10000); %Samplerate für Frequenzen bis fs/2 Hz
-spec=resample(spec,fs,Fs); %Interpoliert/dezimiert das Signal für fs
-
 [fftSize,frameCount] = size(spec);
 windowSize = floor(fftSize/2);
-
-freqs = (0:windowSize)*fs/fftSize;%fs/fftSize=Anzahl Sequenzen pro Sekunde
-times = (1:frameCount) * frameStep/fs;
-
-
-
-% Compute the power.  Easy since positive and negative values are
-
-powerSpec = abs(spec(1:windowSize+1,:));
-
-%multipliziert jede Frequenzenergie mit der zugehörigen Frequenz 
-%(frequenz - samplepunkte entsprechung)
-
-    magSpec = sqrt(powerSpec);
-
-
-    imagesc(times, freqs, real(magSpec'));%magSpec sollte doch sowieso real sein..??
-    axis xy
+freqs = (0:windowSize)*fs/fftSize;%fs/fftSize=Frequenzunschärfe pro fftSample
+times = (1:frameCount) * frameStep/fs;%frameStep/fs=Timestep pro frame
+magSpec = spec(1:windowSize+1,:);
+imagesc(times, freqs, real(magSpec'));
+axis xy
 
 
 
--- feature-generation/Testskript.m
+++ feature-generation/Testskript.m
@@ -6,9 +6,8 @@ clear, clc, close all
 % k, der Redundanzentferner scaled logarithmisch
 
 %einlesen und resampeln
-[y,Fs] = audioread('Bach1.mp3', [100000,800000]);
+[y,Fs] = audioread('Bach1.mp3', [100000,700000]);
 y=sum(y,2);
-length(y)
 fs=2^14;
 y=resample(y,fs,Fs);
 %sound(y,fs);
@@ -19,19 +18,20 @@ h = floor(wlen/4);
 nfft = wlen;
 [stft, f, t] = stft(y, wlen, h, nfft, fs);
 
-
 %Psychoakustische Redundanz entfernen
 S=size(stft);
 k=floor(wlen/10);%die Halbwertssamplelänge (mind. wlen/2)
 rstft=reduce(stft,k);
+size(rstft)
+csvwrite('features.csv',abs(stft'));
 stft=reproduce(rstft,k,S(1));
 Reduktionsfaktor=length(rstft(:,1))/S(1)
 
 %Spektrogramm erzeugen
 MyDisplaySpectrogram(abs(stft'), fs, h);
-csvwrite('features.csv',abs(stft'))
+
 
 %Spektrogramminversion
 [x_istft, t_istft] = istft(stft, h, nfft, fs);
-%sound(x_istft,fs);
+sound(x_istft,fs);
 
